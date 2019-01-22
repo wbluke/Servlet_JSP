5775c77cf74859f58327705ea816597b708fddff
wbluke <pwb0127@naver.com>
2019-01-23 05:02:35 +0900

    first commit

---
+++ src/com/edu/test/FirstServlet.java
@@ -0,0 +1,19 @@
+package com.edu.test;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class FirstServlet extends HttpServlet {
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException{
+		System.out.println("init() 실행됨!");
+	}
+	
+	@Override
+	public void service(ServletRequest arg0, ServletResponse arg1) throws ServletException, IOException{
+		System.out.println("service() 실행됨!");
+	}
+	
+}
