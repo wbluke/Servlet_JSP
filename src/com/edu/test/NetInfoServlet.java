723a1504602bc42e16e8a9423e2433d4c2458f51
wbluke <pwb0127@naver.com>
2019-01-25 01:55:13 +0900

    CH03_HTTPServletRequest & HTTPServletResponse

---
+++ src/com/edu/test/NetInfoServlet.java
@@ -0,0 +1,37 @@
+package com.edu.test;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/netInfo")
+public class NetInfoServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException{
+		res.setContentType("text/html;charset=EUC-KR");
+		PrintWriter out = res.getWriter();
+		out.print("<html>");
+		out.print("<head><title>Request 정보 출력 Servlet</title></head>");
+		out.print("<body>");
+		out.print("<h3>네트워크 관련 요청 정보</h3>");
+		out.print("Request Scheme : " + req.getScheme() + "<br/>");
+		out.print("Server Name : " + req.getServerName() + "<br/>");
+		out.print("Server Address : " + req.getLocalAddr() + "<br/>");
+		out.print("Server Port : " + req.getServerPort() + "<br/>");
+		out.print("Client Address : " + req.getRemoteAddr() + "<br/>");
+		out.print("Client Host : " + req.getRemoteHost() + "<br/>");
+		out.print("Client Port : " + req.getRemotePort() + "<br/>");
+		out.print("</body></html>");
+		out.close();
+	}
+}
+
+//네트워크 관련 요청 정보
+//
+//Request Scheme : http
+//Server Name : localhost
+//Server Address : 0:0:0:0:0:0:0:1
+//Server Port : 8080
+//Client Address : 0:0:0:0:0:0:0:1
+//Client Host : 0:0:0:0:0:0:0:1
+//Client Port : 53545
--- src/com/edu/test/SecondServlet.java
+++ src/com/edu/test/SecondServlet.java
@@ -1,16 +1,19 @@
 package com.edu.test;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/second")  // 서블릿 실행을 위한 URI를 "/second"로 설정 
 public class SecondServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
 		System.out.println("SecondServlet!");
+		PrintWriter out = resp.getWriter();
+		out.print("<html><head><title>Test</title></head>");
+		out.print("<body><h1>have a nice day!!</h1></body>");
+		out.print("</html>");
+		out.close();
 	}
 }
